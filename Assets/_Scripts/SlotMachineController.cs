eb966eaf42e3839ff2d414158aaf0ff72b42f4a3
Naveen Menon <naveenmenon.connect@gmail.com>
2026-08-20 01:40:27 +0530

    Added win game logic

--- Assets/_Scripts/SlotMachineController.cs
+++ Assets/_Scripts/SlotMachineController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SlotMachineController : MonoBehaviour
@@ -7,25 +8,63 @@ public class SlotMachineController : MonoBehaviour
     [SerializeField] private ReelController _reel02;
     [SerializeField] private ReelController _reel03;
 
+    private bool _isSpinning;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !_isSpinning)
         {
-            Spin();
+            StartCoroutine(SpinRoutine());
         }
     }
 
-    private void Spin()
+    private IEnumerator SpinRoutine()
     {
-        if (_reel01.IsSpinning ||
-            _reel02.IsSpinning ||
-            _reel03.IsSpinning)
-        {
-            return;
-        }
+        _isSpinning = true;
 
         _reel01.Spin();
         _reel02.Spin();
         _reel03.Spin();
+
+        // Wait until all three reels finish.
+        while (_reel01.IsSpinning ||
+               _reel02.IsSpinning ||
+               _reel03.IsSpinning)
+        {
+            yield return null;
+        }
+
+        CheckResult();
+
+        _isSpinning = false;
+    }
+
+    private void CheckResult()
+    {
+        SlotSymbol.SymbolType result01 =
+            _reel01.CurrentResult;
+
+        SlotSymbol.SymbolType result02 =
+            _reel02.CurrentResult;
+
+        SlotSymbol.SymbolType result03 =
+            _reel03.CurrentResult;
+
+        bool isWin =
+            result01 == result02 &&
+            result02 == result03;
+
+        Debug.Log(
+            $"Results: {result01} | {result02} | {result03}"
+        );
+
+        if (isWin)
+        {
+            Debug.Log("WIN!");
+        }
+        else
+        {
+            Debug.Log("NO WIN");
+        }
     }
 }
